3658857b37ff283fa9a6a4da986168a258c2f4e7
Nehemjit Rai <rain3@mymacewan.ca>
2022-05-09 00:35:39 -0600

    Add mobile first approach

--- fade.js
+++ fade.js
@@ -3,7 +3,7 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     
-    document.getElementById("navbar").style.background = "#3a3b3c";
+    document.getElementById("navbar").style.background = "#18191a";
     document.getElementById("navbar").style.opacity = "0.95";
   } else {
    
