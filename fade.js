c4d853ff5364c49a785cd32e37f5ef8ad1131172
Nehemjit Rai <rain3@mymacewan.ca>
2022-05-12 23:31:32 -0600

    Update project pages

--- fade.js
+++ fade.js
@@ -4,9 +4,10 @@ function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     
     document.getElementById("navbar").style.background = "#18191a";
-    document.getElementById("navbar").style.opacity = "0.95";
+    document.getElementById("collapse").style.background = "#18191a";
   } else {
    
     document.getElementById("navbar").style.background = "none";
+    document.getElementById("collapse").style.background = "none";
   }
 }
