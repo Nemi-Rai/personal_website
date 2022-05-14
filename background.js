6a1f17d5f0dfc2c7f0dd84bedd8750c7201c9bf1
Nehemjit Rai <rain3@mymacewan.ca>
2022-05-14 03:01:09 -0600

    Fix navbar dropdown

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 function background() {
-    if (document.getElementById("collapse").ariaExpanded == false) {
+    if (document.getElementById("navbutton").ariaExpanded == false) {
         document.getElementById("collapse").style.backgroundColor = "none";
     } else {
         document.getElementById("collapse").style.backgroundColor = "#18191a";
--- fade.js
+++ fade.js
@@ -2,12 +2,8 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    
     document.getElementById("navbar").style.background = "#18191a";
-    document.getElementById("collapse").style.background = "#18191a";
   } else {
-   
     document.getElementById("navbar").style.background = "none";
-    document.getElementById("collapse").style.background = "none";
   }
 }
