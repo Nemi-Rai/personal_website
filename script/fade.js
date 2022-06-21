e619437a979886dc085cdb74aeb9fec39e15175d
Nehemjit Rai <rain3@mymacewan.ca>
2022-06-21 01:31:47 -0600

    Fix mobile nav bar
    background were transparent and not user friendly.

--- script/fade.js
+++ script/fade.js
@@ -3,7 +3,11 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("navbar").style.background = "#050505";
+    document.getElementById("navbutton").style.display = "block";
+    document.getElementById("home").style.display = "inline";
   } else {
     document.getElementById("navbar").style.background = "none";
+    document.getElementById("navbutton").style.display = "none";
+    document.getElementById("home").style.display = "none";
   }
 }
