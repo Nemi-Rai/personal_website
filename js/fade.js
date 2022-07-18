4172651dae4ce932db49685377b12a295ff124e0
Nehemjit Rai <rain3@mymacewan.ca>
2022-07-18 10:55:58 -0600

    Add automotive networking
    Renamed hacking cars

--- js/fade.js
+++ js/fade.js
@@ -4,10 +4,8 @@ function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("navbar").style.background = "#050505";
     document.getElementById("navbutton").style.display = "block";
-    document.getElementById("home").style.display = "inline";
   } else {
     document.getElementById("navbar").style.background = "none";
     document.getElementById("navbutton").style.display = "none";
-    document.getElementById("home").style.display = "none";
   }
 }
